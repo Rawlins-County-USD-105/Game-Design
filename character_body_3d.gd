134649f16e58f69f64fc1be84ae233fa9ad3a72c
basalpear <asapopp@student.usd105.org>
2024-10-17 11:32:01 -0500

    Updates

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -17,6 +17,12 @@ const FOV_CHANGE = 1.5
 @onready var neck: Node3D = $neck
 @onready var camera: Camera3D = $neck/Camera
 
+func _enter_tree() -> void:
+	set_multiplayer_authority(name.to_int())
+	
+func _ready() -> void:
+	camera.current = is_multiplayer_authority()
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -28,45 +34,48 @@ func _unhandled_input(event: InputEvent) -> void:
 			camera.rotate_x(-event.relative.y * 0.01)
 			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(60))
 func _physics_process(delta: float) -> void:
-	 
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	if is_multiplayer_authority():
+		# Add the gravity.
+		if not is_on_floor():
+			velocity += get_gravity() * delta
+			
+		# Handle jump.
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 		
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		if Input.is_action_just_pressed("ui_end"):
+			$"../".exit_game(name.to_int())
 
-	# Handle Sprint.
-	if Input.is_action_pressed("sprint"):
-		speed = SPRINT_SPEED
-	else:
-		speed = WALK_SPEED
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("left", "right", "forward", "back")
-	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if is_on_floor():
-		if direction:
-			velocity.x = direction.x * speed
-			velocity.z = direction.z * speed
+		# Handle Sprint.
+		if Input.is_action_pressed("sprint"):
+			speed = SPRINT_SPEED
 		else:
-			velocity.x = move_toward(velocity.x, 0, speed)
-			velocity.z = move_toward(velocity.z, 0, speed)
-	else:
-		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
-		velocity.z = lerp(velocity.z, direction.x * speed, delta * 3.0)
+			speed = WALK_SPEED
+
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var input_dir := Input.get_vector("left", "right", "forward", "back")
+		var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		if is_on_floor():
+			if direction:
+				velocity.x = direction.x * speed
+				velocity.z = direction.z * speed
+			else:
+				velocity.x = move_toward(velocity.x, 0, speed)
+				velocity.z = move_toward(velocity.z, 0, speed)
+		else:
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
+			velocity.z = lerp(velocity.z, direction.x * speed, delta * 3.0)
+			
+	#Heahead bob
+		t_bob += delta * velocity.length() * float(is_on_floor())
+		camera.transform.origin = _headbob(t_bob)
+		move_and_slide()
 		
-#Heahead bob
-	t_bob += delta * velocity.length() * float(is_on_floor())
-	camera.transform.origin = _headbob(t_bob)
-	move_and_slide()
-	
-	#FOV
-	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
-	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
-	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
+		#FOV
+		var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
+		var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
+		camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
 func _headbob(time) -> Vector3:
 	var pos = Vector3.ZERO
 	pos.y = sin(time * BOB_FREQ) * BOB_AMP
--- game.gd
+++ game.gd
@@ -5,8 +5,31 @@ var peer = ENetMultiplayerPeer.new()
 @export var playe_scene : PackedScene
 
 func _on_host_pressed() -> void:
-	pass # Replace with function body.
-
-
+	peer.create_server(1027)
+	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(add_player)
+	add_player()
+	$CanvasLayer.hide()
+	
 func _on_join_pressed() -> void:
-	pass # Replace with function body.
+	peer.create_client("127.0.0.1", 1027)
+	multiplayer.multiplayer_peer = peer
+	$CanvasLayer.hide()
+
+func add_player(id = 1):
+	var player = playe_scene.instantiate()
+	player.name = str(id)
+	call_deferred("add_child")
+	
+
+func del_player(id):
+	rpc("_del_player", id)
+	
+@rpc("any_peer", "call_local")
+
+func _del_player(id):
+	get_node(str(id)).queue_free()
+	
+func exit_game(id):
+	multiplayer.peer_disconnected.connect(del_player)
+	del_player(id)
