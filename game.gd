a19f61fc103cc6c91b11be1f3795e96bdeed7946
basalpear <asapopp@student.usd105.org>
2024-10-17 10:39:28 -0500

    Multiplayer

---
+++ game.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+var peer = ENetMultiplayerPeer.new()
+
+@export var playe_scene : PackedScene
+
+func _on_host_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_join_pressed() -> void:
+	pass # Replace with function body.
