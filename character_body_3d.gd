4b951e8177627e92887b9d1616492a10cbc24462
BigGuy333 <ryansramek@student.usd105.org>
2024-10-21 10:25:40 -0500

    crouching

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -1,15 +1,30 @@
 extends CharacterBody3D
 
 var speed
+
+
+
+@onready var standing_collison_shape: CharacterBody3D = $"."
+
+@onready var crouching_collision_shape: CollisionShape3D = $crouching_collision_shape
+
+var current_speed = 5.0
+
 const WALK_SPEED = 5.0
 const SPRINT_SPEED = 8.0
+var  crouching_speed = 0.6
 const JUMP_VELOCITY = 5.0
 const SENSITIVITY = 0.005
 
-#Bob# BOB Variables
+var lerp_speed = 30
+
+#Bob 
 const BOB_FREQ = 2.0
 const BOB_AMP = 0.08
 var t_bob = 0.0
+
+# Crouching
+var crouching_depth = -0.5
 
 #Fov Variables
 const BASE_FOV = 75.0
@@ -36,19 +51,32 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Handle Sprint.
-	if Input.is_action_pressed("sprint"):
-		speed = SPRINT_SPEED
+	#crouching
+	if Input.is_action_pressed("crouch"):
+		current_speed = crouching_speed
+		neck.position.y = lerp(neck.position.y, 1.006 + crouching_depth, delta * lerp_speed)
+		standing_collison_shape.set_deferred("disable",true)
+		crouching_collision_shape.set_deferred("disable",false)
 	else:
-		speed = WALK_SPEED
-
+		standing_collison_shape.set_deferred("disable", false)
+		crouching_collision_shape.set_deferred("disable", true)
+		neck.position.y = lerp(neck.position.y, 1.006, delta * lerp_speed)
+		
+		if Input.is_action_pressed("sprint"):
+			speed = SPRINT_SPEED
+		else:
+			speed = WALK_SPEED
+	if Input.is_action_pressed("crouch"):
+		speed = WALK_SPEED * crouching_speed
+		
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "back")
 	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if is_on_floor():
 		if direction:
+			
 			velocity.x = direction.x * speed
 			velocity.z = direction.z * speed
 		else:
