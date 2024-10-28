1ecdbef165a64b0c702bfb8bddd59e5531f78fd7
basalpear <asapopp@student.usd105.org>
2024-10-28 10:31:31 -0500

    Update game.gd

--- game.gd
+++ game.gd
@@ -11,6 +11,7 @@ func _on_host_pressed() -> void:
 	peer.create_server(PORT)
 	multiplayer.multiplayer_peer = peer
 	multiplayer.peer_connected.connect(add_player)
+	multiplayer.peer_disconnected.connect(del_player)
 	add_player(multiplayer.get_unique_id())
 	$CanvasLayer.hide()
 	
@@ -25,17 +26,10 @@ func add_player(peer_id):
 	add_child(player)
 	
 
-func del_player(id):
-	rpc("_del_player", id)
-	
-@rpc("any_peer", "call_local")
-
-func _del_player(id):
-	get_node(str(id)).queue_free()
-	
-func exit_game(id):
-	multiplayer.peer_disconnected.connect(del_player)
-	del_player(id)
+func del_player(peer_id):
+	var player = get_node_or_null(str(peer_id))
+	if player:
+		player.queue_free()
 
 func _on_line_edit_text_submitted(new_text: String) -> void:
 	pass # Replace with function body.
