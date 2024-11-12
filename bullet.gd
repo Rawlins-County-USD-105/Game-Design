56efcb46ea747ec6b1edcab0732ec9dca0bc16c7
ShaunLare <shaunl2026@student.usd105.org>
2024-11-12 10:04:43 -0600

    weapon

--- bullet.gd
+++ bullet.gd
@@ -19,9 +19,6 @@ func _process(delta):
 	if ray.is_colliding():
 		mesh.visible = false
 		particles.emitting = true
-		ray.enabled = false
-	if ray.get_collider().is_in_group("Enemy"):
-		ray.get_Collider().hit()
 		await get_tree().create_timer(1.0).timeout
 		queue_free()
 
