5a7da427aa956aadeaedc2db19a33c0dd419a15a
basalpear <asapopp@student.usd105.org>
2024-11-19 10:30:05 -0600

    updates

--- melee_weapon.gd
+++ melee_weapon.gd
@@ -1,13 +1,13 @@
 extends Node3D
 
-class_name Weapon
+class_name melee
 
 @export var damage: int
 @export var speed: int
 @export var weapon_anim: AnimationPlayer
 @export var model: MeshInstance3D
 @export var barrel: RayCast3D
-@export var gun_bullet: PackedScene
+
 var instance
 
 func _ready() -> void:
@@ -20,7 +20,4 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("Pew") and not weapon_anim.is_playing():
 		print("works")
 		weapon_anim.play("Pew")
-		instance = gun_bullet.instantiate()
-		instance.position = barrel.global_position
-		instance.transform.basis = barrel.global_transform.basis
 		get_parent().add_child(instance)
