d3c0f4a4d6302ca690f4a20fa72b166a2cf483a8
JeremiahLare09 <jeremiahl2027@student.usd105.org>
2024-11-20 10:41:21 -0600

    Door
    The door opens

--- Game Interactions/Scripts/Interactable.gd
+++ Game Interactions/Scripts/Interactable.gd
@@ -1,9 +1,11 @@
-extends CollisionObject3D
+extends Node3D
 class_name Interactable
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 @export var prompt_message = "Interact"
 @export var prompt_input = "interact" 
-@onready var collision_shape_3d: CollisionShape3D = $"../StaticBody3D/CollisionShape3D"
+@onready var collision_shape_3d: CollisionShape3D = $Door_1_grp/Door_1/CollisionShape3D
+@onready var timer: Timer = $Timer
 
 
 
@@ -11,4 +13,5 @@ func get_prompt():
 	var key_name = "Menu"
 func Interact(body):
 	print(body.name, " interacted with ", name)
-	queue_free()
+	animation_player.play("Door Open")
+	
