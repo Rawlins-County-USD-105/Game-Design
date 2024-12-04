e20f40f2ab0ad082c85578cc1d6f40e88cbac705
EJAnder2026 <erikanderson@student.usd105.org>
2024-12-04 10:22:47 -0600

    Fixes
    THums up

--- enemy/bean.gd
+++ enemy/bean.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
 
-var player = null
+var player= null
 
 const SPEED = 3.0
 
@@ -22,7 +22,6 @@ func _process(_delta):
 	nav_agent.set_target_position(player.global_transform.origin)
 	var next_nav_point = nav_agent.get_next_path_position()
 	velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
-	
 	look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
 	
 	move_and_slide()
