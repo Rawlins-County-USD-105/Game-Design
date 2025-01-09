d8e59464c03af57c510fd18888595b3498324352
EJAnder2026 <erikanderson@student.usd105.org>
2025-01-09 10:21:19 -0600

    enemy spawning
    dumb

--- Models/game.gd
+++ Models/game.gd
@@ -4,11 +4,11 @@ extends Node3D
 @onready var current_level=0
 #the level is the key the amount of monsters is the value
 @onready var monster_dict={
- 1:20,
- 2:2,
- 3:4,
- 4:8,
- 5:30
+	1:20,
+	2:2,
+	3:4,
+	4:8,
+	5:30
 }
 
 #the monster we will be spawning in. 
@@ -24,24 +24,25 @@ func enemy_death():
 	print("enemy death")
 	dead_enemies+=1
 	if dead_enemies==monster_dict[current_level]:
-		$InBetweenWaves.start()
+		print("start wave")
+		$WaveTimer.start()
 		dead_enemies=0
-		get_tree().call_group("player", "award_points", 200)
+	get_tree().call_group("player", "award_points", 200)
 
 func spawn_enemies():
 	for i in range(monster_dict[current_level]):
 		var m = monster.instantiate()
 		print("spawning enemy")
-  #we check the amount of children on our spawn holder 
+		#we check the amount of children on our spawn holder 
 		var spawn_length = $SpawnHolder.get_child_count()-1
 		var rand_num = rand.randi_range(0,spawn_length)
-  #We use that number to randomly select a spawner node position to use 
+		#We use that number to randomly select a spawner node position to use 
 		var spawn_postion = $SpawnHolder.get_child(rand_num).position
-  #we add the monster as a child of the level
-  #We set the monsters position to the spawn location
+		#we add the monster as a child of the level
+		#We set the monsters position to the spawn location
 		m.position = spawn_postion
 		add_child(m)
-  #This is just a fast way to create a timer similar to wait in python
+		#This is just a fast way to create a timer similar to wait in python
 		await get_tree().create_timer(2.0).timeout
   
 
@@ -49,7 +50,7 @@ func update_level(level):
 	match level:
 		1:
 			print("its level one")
-   #additional stuff here like announcer saying: Wave 1!
+			#additional stuff here like announcer saying: Wave 1!
 		2:
 			print("its level two")
 		3:
@@ -62,7 +63,7 @@ func update_level(level):
 
 
 
-func _on_in_between_waves_timeout():
+func _on_wave_timer_timeout():
 	print("Leaving level: ", current_level)
 	current_level+=1
 	update_level(current_level)
