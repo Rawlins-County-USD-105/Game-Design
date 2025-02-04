3f85cd3f9bafc4b42e7001ecaee16c21e90bbe2a
GrainPourage <andrewanderson@student.usd105.org>
2025-02-04 11:52:22 -0600

    water works

--- Scripts/button.gd
+++ Scripts/button.gd
@@ -1,6 +1,7 @@
 extends Interactable
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-
+var Water = 100
 #this will play will the button is interacted with
 func _on_interacted(body: Variant) -> void:
 	animation_player.play("press")
+	Gain.Gain_Water(Water)
