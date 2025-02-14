de7c80e5e4ae83d85835095ac9308ca9e6a5eeb7
basalpear <asapopp@student.usd105.org>
2025-02-14 11:27:46 -0600

    audio

--- Scripts/character_body_3d.gd
+++ Scripts/character_body_3d.gd
@@ -13,6 +13,7 @@ var health = max_health
 @onready var energybar: ProgressBar = $neck/Camera/TextureRect/Energybar
 @onready var damagebar: ProgressBar = $neck/Camera/TextureRect/Healthbar/Damagebar
 @onready var damage_bar_timer: Timer = $neck/Camera/TextureRect/Healthbar/DamageBarTimer
+@onready var audio: AudioStreamPlayer3D = $AudioStreamPlayer3D
 
 @export_category("Movement and shiz")
 @export var mousesense = 1
@@ -98,6 +99,7 @@ func took_damage(Damage):
 	if health <= 0:
 		damagebar.value = 0
 		print("You Died")
+		audio.play()
 	
 	healthbar.value = health
 	regen.start()
