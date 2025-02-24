651bd3153b524f1dc917f52336428a7b8b2d262a
basalpear <asapopp@student.usd105.org>
2025-02-24 11:41:16 -0600

    Update weapon.gd

--- Scripts/weapon.gd
+++ Scripts/weapon.gd
@@ -8,8 +8,11 @@ class_name Weapon
 var instance
 
 func gun(current_gun, animation_player, animation_name):
-	if current_gun.visible == false:
-		if Input.is_action_pressed("Pew") and not animation_player.is_playing():
+	if current_gun.visible == true:
+		if Input.is_action_pressed(animation_name) and not animation_player.is_playing():
+			
+			Gain.Use_Water(10)
+			
 			animation_player.play(animation_name)
 			instance = gun_bullet.instantiate()
 			instance.position = barrel.global_position
