e930a563b30f05f839fb091a2d8804cafbadce33
ShaunLare <shaunl2026@student.usd105.org>
2025-03-05 11:01:56 -0600

    FIxes

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -13,7 +13,7 @@ class_name bullet
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func bullet_function(delta):
 
 	#print(Watergun)
 	position += transform.basis * Vector3(0, 0, -speed) * delta
