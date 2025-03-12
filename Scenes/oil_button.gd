3ea6902a7df4a7c644ddc023f327dda218e23aef
basalpear <asapopp@student.usd105.org>
2025-03-12 10:44:09 -0500

    Drill

--- Scenes/oil_button.gd
+++ Scenes/oil_button.gd
@@ -1,6 +1,8 @@
 extends Interactable
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animation_player_drill: AnimationPlayer = $"../AnimationPlayer"
 
 func _on_interacted(body: Variant) -> void:
 	body.spawning = true
 	animation_player.play("press")
+	animation_player_drill.play("drill")
