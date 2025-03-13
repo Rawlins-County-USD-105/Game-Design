fa39f75ba8cd7b274a7bba69d9996a1b893b1fe4
basalpear <asapopp@student.usd105.org>
2025-03-13 10:40:30 -0500

    guh

--- Scenes/oil_button.gd
+++ Scenes/oil_button.gd
@@ -5,7 +5,7 @@ extends Interactable
 @onready var label: Label = $"../OilBar/Label"
 
 var oil = 0
-var max_oil = 100
+var max_oil = 1000
 
 func _on_interacted(body: Variant) -> void:
 	body.spawning = true
@@ -15,6 +15,8 @@ func _on_interacted(body: Variant) -> void:
 func get_oil(player):
 	var label_text = str(oil_bar.value / oil_bar.max_value)
 	label.text = label_text + "%"
+	
+	
 	
 	if oil_bar.value == oil_bar.max_value:
 		player.spawning = false
