b82aeb71a11bec3b18e0ee103b14d2f8eb23be7f
basalpear <asapopp@student.usd105.org>
2025-03-26 10:42:03 -0500

    updates

--- Scenes/oil_button.gd
+++ Scenes/oil_button.gd
@@ -2,10 +2,10 @@ extends Interactable
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var animation_player_drill: AnimationPlayer = $"../AnimationPlayer"
 @onready var oil_bar: ProgressBar = $"../OilBar"
-@onready var label: Label = $"../OilBar/Label"
 @onready var timer: Timer = $"../Timer"
 @onready var barrels: Label = $"../Barrels"
 @onready var gpu_particles_3d: GPUParticles3D = $"../GPUParticles3D"
+@onready var health_bar_sprite: Node3D = $"../Health"
 
 var oil = 0
 var max_oil = 1000
@@ -14,12 +14,13 @@ var barrel = 0
 
 func _ready() -> void:
 	oil_bar.hide()
-	label.text = ""
+	health_bar_sprite.hide()
 	oil_bar.max_value = max_oil
 	oil_bar.value = 0
 
 func _on_interacted(body: Variant) -> void:
 	oil_bar.show()
+	health_bar_sprite.show()
 	player = body
 	player.spawning = true
 	animation_player.play("press")
@@ -28,12 +29,10 @@ func _on_interacted(body: Variant) -> void:
 
 func get_oil():
 	var label_text = str(oil_bar.value / oil_bar.max_value * 100)
-	label.text = label_text + "%"
 	
 	oil_bar.value = oil
 	
 	if oil_bar.value == oil_bar.max_value:
-		label.text = "Gathered Oil Barrel"
 		barrel += 1
 		barrels.text = str(barrel)
 		player.spawning = false
@@ -41,7 +40,6 @@ func get_oil():
 		oil = 0
 		oil_bar.value = oil
 		oil_bar.hide()
-		label.text = ""
 
 
 func _on_timer_timeout() -> void:
