c7099f6a2b6dda4d6135eae11da2edcd5b6678dd
GrainPourage <andrewanderson@student.usd105.org>
2025-03-27 10:39:20 -0500

    eh

--- Models/game.gd
+++ Models/game.gd
@@ -1,12 +1,14 @@
 extends Node3D
+@onready var spawn_1: Area3D = $SpawnZones/spawn1
 
 @onready var spawn_zones = {$SpawnZones/spawn1:1, $SpawnZones/spawn2:2}
-
+var total_enemies = 0
+var enemies_spawned = 0
 func _process(delta: float) -> void:
-	pass
+	spawning()
 			
 func spawning():
 	for x in spawn_zones:
 		var rand = randi_range(1,2)
 		if x:
-			pass
+			print(x.position)
