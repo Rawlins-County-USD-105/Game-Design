8ec733efdd361414b0542e331856d1de6826d9df
JeremiahLare09 <jeremiahl2027@student.usd105.org>
2025-03-31 10:38:20 -0500

    Renaming and small bug fixes for stuff

--- Scripts/water_gun.gd
+++ Scripts/water_gun.gd
@@ -1,6 +1,8 @@
 extends Weapon
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var popup_menu: Node2D = $"../../../Popup menu"
 
 func _process(delta: float) -> void:
-	gun(self, animation_player, "Pew")
+	if popup_menu.visible == false:
+		gun(self, animation_player, "Pew")
