2fcc7d5b25f4ea75b6338378ba1904c3858a16bf
BigGuy333 <ryansramek@student.usd105.org>
2025-03-31 10:18:29 -0500

    Revert "Merge pull request #75 from Rawlins-County-USD-105/ryanbranch"
    This reverts commit ed06d52409494e579a2ff037f628fa48ab9514ea, reversing
    changes made to 14cd573cff39c4ad677f1886262665c4ebcc403e.

--- Scenes/oil_button.gd
+++ Scenes/oil_button.gd
@@ -5,15 +5,12 @@ extends Interactable
 @onready var timer: Timer = $"../Timer"
 @onready var barrels: Label = $"../Barrels"
 @onready var gpu_particles_3d: GPUParticles3D = $"../GPUParticles3D"
-@onready var begin_drill: AudioStreamPlayer3D = $"../begin_drill"
-@onready var end_drill: AudioStreamPlayer3D = $"../end_drill"
 @onready var health_bar_sprite: Node3D = $"../Health"
 
 var oil = 0
 var max_oil = 1000
 var player = null
 var barrel = 0
-var sound_playing = false
 
 func _ready() -> void:
 	oil_bar.hide()
@@ -24,11 +21,6 @@ func _process(delta: float) -> void:
 	if player:
 		health_bar_sprite.look_at(player.global_transform.origin, Vector3.UP)
 
-func _process(delta: float) -> void:
-	if sound_playing and not begin_drill.playing and not end_drill.playing:
-		end_drill.play()
-		
-
 func _on_interacted(body: Variant) -> void:
 	oil_bar.show()
 	health_bar_sprite.show()
@@ -36,10 +28,8 @@ func _on_interacted(body: Variant) -> void:
 	player.spawning = true
 	animation_player.play("press")
 	animation_player_drill.play("drill")
-	sound_playing = true
 	timer.start()
-	begin_drill.play()
-	
+
 func get_oil():
 	var label_text = str(oil_bar.value / oil_bar.max_value * 100)
 	
@@ -52,7 +42,6 @@ func get_oil():
 		await get_tree().create_timer(3).timeout
 		oil = 0
 		oil_bar.value = oil
-		sound_playing = false
 		oil_bar.hide()
 
 
@@ -64,4 +53,3 @@ func _on_timer_timeout() -> void:
 	else:
 		animation_player_drill.stop()
 		gpu_particles_3d.emitting = false
-	
