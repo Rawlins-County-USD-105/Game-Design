25cd8b33a7de1a488b1eee5d7c3f7b8bd15cd71b
ShaunLare <shaunl2026@student.usd105.org>
2025-03-31 10:33:08 -0500

    updates

--- Scenes/shop_button.gd
+++ Scenes/shop_button.gd
@@ -1,7 +1,8 @@
 extends "res://Scripts/interacable.gd"
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var skill_tree: Control = $SkillTree
 
 
 func _on_interacted(body: Variant) -> void:
 	animation_player.play("press")
-	
+	skill_tree.visible = true
