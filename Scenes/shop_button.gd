0901e8390a52a75e35aef1fc195675afcc06d70e
ShaunLare <shaunl2026@student.usd105.org>
2025-04-03 10:15:06 -0500

    skibidi rizz gyatt ohio

--- Scenes/shop_button.gd
+++ Scenes/shop_button.gd
@@ -1,10 +1,15 @@
 extends Interactable
+
+
+# Called when the node enters the scene tree for the first time.
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var skill_tree: Control = $SkillTree
 
-func _on_button_interact(body: Variant) -> void:
-	animation_player.play("push")
-	if skill_tree.visible == true:
-		skill_tree.visible = false
-	else:
+# Called when the node enters the scene tree for the first time.
+func _on_interacted(body: Variant) -> void:
+	animation_player.play("press")
+	if skill_tree.visible == false:
 		skill_tree.visible = true
+	else:
+		skill_tree.visible = false
+	
--- skill_button.gd
+++ skill_button.gd
@@ -2,9 +2,10 @@ extends Interactable
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var skill_tree: Control = $SkillTree
 
-var Water = 100
 #this will play will the button is interacted with
 func _on_interacted(body: Variant) -> void:
 	animation_player.play("press")
-	Gain.Gain_Water(Water)
-	Gain.Gain_Gold(Water)
+	if skill_tree.visible == false:
+		skill_tree.visible = true
+	else:
+		skill_tree.visible = false
