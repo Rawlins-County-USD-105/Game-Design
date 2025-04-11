bc99850973cea6762eeae320219b67ecccb5be9b
basalpear <asapopp@student.usd105.org>
2025-04-11 12:17:25 -0500

    gun

--- Sounds/railme.gd
+++ Sounds/railme.gd
@@ -1,6 +1,6 @@
 extends Weapon
 
-@onready var animation_player: AnimationPlayer = $"../AnimationPlayer"
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 
