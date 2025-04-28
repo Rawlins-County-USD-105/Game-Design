74e2010a15bf1cd50b9a4d716a934fc2e504ca99
ShaunLare <shaunl2026@student.usd105.org>
2025-04-28 10:15:37 -0500

    Skill Tree Update

--- Scripts/pistol.gd
+++ Scripts/pistol.gd
@@ -3,5 +3,10 @@ extends Weapon
 @onready var pistol_anim: AnimationPlayer = $pistol_anim
 
 
+@onready var popup_menu: Node2D = $"../../../Popup menu"
+@onready var skill_tree: Control = $"../../../SkillTree"
+@onready var skill_tree_2: Control = $"../../../SkillTree2"
+
 func _process(delta: float) -> void:
-	gun(self, pistol_anim, "fire")
+	if popup_menu.visible == false and skill_tree.visible == false and skill_tree_2.visible == false:
+		gun(self, pistol_anim, "fire")
