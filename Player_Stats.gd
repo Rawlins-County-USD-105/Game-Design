c0390ef7d873e7f5a2ef9f214e5c9ff25575b767
ShaunLare <shaunl2026@student.usd105.org>
2025-05-12 09:53:16 -0500

    sure

--- Player_Stats.gd
+++ Player_Stats.gd
@@ -1,11 +1,31 @@
 extends Node
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+signal stats_updated
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+enum upgrades {ADD_HEALTH, ADD_SPEED, COLLECTOR_SPEED}
+@onready var collector_speed: Timer = $"../Timer"
+var addhealth = 0
+var addspeed = 0
+var addcollector_speed = 0
+func add_upgrade(upgrade, stat):
 	pass
+	
+func reset_upgrade():
+	addhealth = 0
+	addspeed = 0
+	addcollector_speed = 0
+	
+func get_upgraded_health():
+	return health + addhealth
+	
+func get_upgraded_speed():
+	return SPEED + addspeed
+	
+func get_upgraded_collector_speed():
+	return collector_speed - addcollector_speed / 10.0
+	
+
+
+func _on_stats_updated() -> void:
+	pass # Replace with function body.
